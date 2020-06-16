154825804c48e85be2268a5967e9b0b67ea01923
Rahim Alwer <rahim_alwer@hotmail.com>
2020-06-16 18:26:18 +1000

    chore: install typeorm sqlite

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,6 +4,8 @@ import { bootstrap } from './bootstrap';
 import { coreModules } from './modules';
 import { ModuleProvider } from './support/ModuleProvider';
 
+// @TODO: Export -> DIToken | Config | {Module}Event | Generic Data Types
+
 export default async function auth(
   config?: Config,
   modules?: ModuleProvider<any>[],
